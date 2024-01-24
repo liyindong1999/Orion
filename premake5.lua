15a29da6f337be0fa8d5b39dcaf47d32dd95b6a3
liyindong <2397619613@qq.com>
2024-01-24 16:45:16 +0800

    add premake

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Orion"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Orion"
+	location "Orion"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("Intermediate/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/ThirdParty/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.19041.0"
+
+		defines
+		{
+			"ORION_PLATFORM_WINDOWS",
+			"ORION_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "ORION_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "ORION_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "ORION_DIST"
+		optimize "On"
+
+project	"Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("Intermediate/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Orion/ThirdParty/spdlog/include",
+		"Orion/src"
+	}
+
+	links
+	{
+		"Orion"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.19041.0"
+
+		defines
+		{
+			"ORION_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "ORION_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "ORION_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "ORION_DIST"
+		optimize "On"
